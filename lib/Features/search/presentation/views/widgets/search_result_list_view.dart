ed3d1695cc2c6f85481eec7c4a135e338d618c89
ismaiel <asmaiel649@gmail.com>
2025-02-20 17:15:26 +0300

    UI done

---
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -0,0 +1,21 @@
+
+import 'package:bookly/Features/home/presentation/views/widgets/book_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero, // إزالة الهوامش الداخلية للقائمة
+      itemCount: 10, // عدد العناصر في القائمة
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10), // هوامش رأسية بين العناصر
+          child: BookListViewItem(), // عرض عنصر الكتاب
+        );
+      },
+    );
+  }
+}
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,5 @@
-import 'package:bookly/Features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:bookly/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/Features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -12,43 +12,29 @@ class SearchViewBody extends StatelessWidget {
       // تطبيق هوامش أفقية ثابتة
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment:
+            CrossAxisAlignment.start, // محاذاة العناصر إلى اليسار
         children: const [
           // عرض حقل البحث المخصص
           CustomSearchTextField(),
           SizedBox(
             height: 16,
           ),
+          // نص لعرض عنوان "Search Result"
           Text(
             "Search Result",
-            style: Styles.textstyle18,
+            style: Styles
+                .textstyle18, // استخدام النمط المحدد في Styles.textstyle18
           ),
           SizedBox(
             height: 16,
           ),
+          // قائمة نتائج البحث
           Expanded(
             child: SearchResultListView(),
-          )
+          ),
         ],
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: BookListViewItem(),
-        );
-      },
     );
   }
 }
