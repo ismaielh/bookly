e46e8a0b51b67388b329088563cec9ef0c8b4375
ismaiel <asmaiel649@gmail.com>
2025-02-20 14:24:19 +0300

    add rating to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/CustomBookImage.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -36,7 +37,13 @@ class BookDetailsViewBody extends StatelessWidget {
               style: Styles.textstyle18.copyWith(
                   fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
             ),
-          )
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
         ],
       ),
     );
--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         // أيقونة النجمة لتقييم الكتاب
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 14,
           color: Color(0xffFFDD4F),
         ),
         // إضافة مسافة بين النجمة والنص
@@ -28,9 +31,12 @@ class BookRating extends StatelessWidget {
           width: 5,
         ),
         // نص يعرض عدد المراجعات
-        Text(
-          "(254)",
-          style: Styles.textstyle14.copyWith(color: const Color(0xff707070)),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            "(254)",
+            style: Styles.textstyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
         )
       ],
     );
