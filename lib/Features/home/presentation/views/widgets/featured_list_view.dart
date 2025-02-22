1b2fe32f5cb145f927ee76e6f12b3f93623f4618
ismaiel <asmaiel649@gmail.com>
2025-02-23 01:42:07 +0300

    display featured books part 1

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/Features/home/presentation/manger/feature_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/CustomBookImage.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/core/widgets/custom_loaading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -14,19 +18,29 @@ class FeaturedBooksListView extends StatelessWidget {
       onTap: () {
         GoRouter.of(context).push(AppRouter.kBookDetailsView);
       },
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.3,
-        child: ListView.builder(
-          // تعيين الاتجاه الأفقي للتمرير
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            // إضافة const إلى Padding لتحسين الأداء
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomBookImage(),
+      child: BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+        builder: (context, state) {
+          if (state is FeaturedBookSuccess) {
+            return SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: ListView.builder(
+                // تعيين الاتجاه الأفقي للتمرير
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  // إضافة const إلى Padding لتحسين الأداء
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomBookImage(),
+                  );
+                },
+              ),
             );
-          },
-        ),
+          } else if (state is FeaturedBookFailure) {
+            return CustomErrorWidget(errmessage: state.errMessage);
+          } else {
+            return const CustomLoaadingIndicator();
+          }
+        },
       ),
     );
   }
