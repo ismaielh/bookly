5de10d800d1768348a3f527db9705e2be9a471fb
ismaiel <asmaiel649@gmail.com>
2025-02-23 02:20:38 +0300

    cached network image

--- lib/Features/home/presentation/views/widgets/CustomBookImage.dart
+++ lib/Features/home/presentation/views/widgets/CustomBookImage.dart
@@ -1,4 +1,4 @@
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 // تعريف ويدجيت CustomListViewItem من نوع StatelessWidget
@@ -8,17 +8,14 @@ class CustomBookImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // استخدام SizedBox لتحديد ارتفاع العنصر بالنسبة لحجم الشاشة
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          // استخدام اللون الأحمر كخلفية احتياطية
-
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          fit: BoxFit.fill,
         ),
       ),
     );
