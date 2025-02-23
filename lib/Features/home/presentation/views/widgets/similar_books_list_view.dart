36cffb8dc91115225f179c5fb19a1b243032f33b
ismaiel <asmaiel649@gmail.com>
2025-02-23 17:20:18 +0300

    fetch simialar books done

--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,24 +1,41 @@
+import 'package:bookly/Features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/CustomBookImage.dart';
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/core/widgets/custom_loaading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListview extends StatelessWidget {
   const SimilarBooksListview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.16,
-      child: ListView.builder(
-        // تعيين الاتجاه الأفقي للتمرير
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          // إضافة const إلى Padding لتحسين الأداء
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: CustomBookImage(imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf6zoRR_FPG7f2knECoYTgOuETejMYPg71vg&s",),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.16,
+            child: ListView.builder(
+              // تعيين الاتجاه الأفقي للتمرير
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                // إضافة const إلى Padding لتحسين الأداء
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: CustomBookImage(
+                    imageUrl:
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf6zoRR_FPG7f2knECoYTgOuETejMYPg71vg&s",
+                  ),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errmessage: state.errMessage);
+        } else {
+          return const CustomLoaadingIndicator();
+        }
+      },
     );
   }
 }
